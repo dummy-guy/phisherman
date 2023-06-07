a8c931744b7ff259e3c54362d5d459919b2d5c71
abcdior <126326965+abcdior@users.noreply.github.com>
2023-06-07 07:29:24 +0000

    Add files via upload

--- phisherman.sh
+++ phisherman.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
 
-echo "       PPPPPPP  H     H  II   SSSSSS  "
-echo "       P     P  H     H  II  S     "
-echo "       PPPPPPP  HHHHHHH  II    SSSSS    "
-echo "       P        H     H  II         S"
-echo "       P        H     H  II   SSSSS"
-echo
+# Clear the screen
+clear
 
+# Style the prompt
+PS1="\[\e[1m\]\$ \[\e[0m\]"
+
+# Function to display text as if it's being typed
+type_text() {
+  local text="$1"
+  local delay="0.005"
+  local i=0
+
+  while [ $i -lt ${#text} ]; do
+    echo -n "$(expr substr "$text" $i 1)"
+    sleep "$delay"
+    i=$((i + 1))
+  done
+
+  echo
+}
+
+# Display the ASCII art with typewriter effect
+type_text "            ------------------------------------  "
+
+type_text "            |           PHISHERMAN             |  "
+
+type_text "            ------------------------------------  "
+type_text "            | Tool          | PhisherMan       |  "
+type_text "            | Purpose       | Phishing         |  "
+type_text "            | Author        | ABC Dior         |  "
+type_text "            | Collaborators | None             |  "
+type_text "            | Version       | 1.0              |  "
+type_text "            ------------------------------------  "
+# Execute the Python script
 python host.py
