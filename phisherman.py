c8d58bc30b24ebc8c74988659eac1ee267e4f0eb
dummy-guy <148836695+dummy-guy@users.noreply.github.com>
2023-11-16 20:16:36 +0000

    Add files via upload

--- phisherman.py
+++ phisherman.py
@@ -4,16 +4,24 @@ import json
 from color_functions import type_text, display_template, index_str
 from color_functions import cyan as cyan, magenta as magenta, reset as reset, yellow as yellow, green as green, light_blue as light_blue
 
+def check_installed(command):
+    try:
+        subprocess.check_output(command, stderr=subprocess.STDOUT)
+        return True
+    except subprocess.CalledProcessError:
+        return False
 
 # Clear the screen
 subprocess.run(['clear'])
 
-# Read templates from JSON manifest
+# Read manifest from JSON
 with open('manifest.json', 'r') as manifest_file:
     manifest = json.load(manifest_file)
     templates = manifest.get('templates', [])
+    requirements = manifest.get('requirements', [])
+    python_dependencies = manifest.get('python_dependencies', [])
 
-# Display the ASCII art
+# Display ASCII art
 with open('doom.txt', 'r') as doom_file:
     doom_content = doom_file.read()
     print(f"{green}{doom_content}{reset}")
@@ -29,10 +37,27 @@ print(f"{light_blue}Type template number to select it.{reset}")
 type_text(f"{yellow}For example, type: 01 to select Facebook{reset}")
 type_text(f"")
 
+# Loop through templates
 for i, template in enumerate(templates, start=1):
     indexed_str = index_str(i)
     display_template(indexed_str, template)
     print()
+
+# Check if Linux packages are installed
+not_installed_requirements = [pkg for pkg in requirements if not check_installed(['dpkg', '-s', pkg])]
+if not_installed_requirements:
+    type_text("Installing Linux packages...")
+    subprocess.run(['sudo', 'apt-get', 'install'] + not_installed_requirements)
+else:
+    type_text("Linux packages are already installed.")
+
+# Check if Python dependencies are installed
+not_installed_dependencies = [dep for dep in python_dependencies if not check_installed(['pip3', 'show', dep])]
+if not_installed_dependencies:
+    type_text("Installing Python dependencies...")
+    subprocess.run(['pip3', 'install'] + not_installed_dependencies)
+else:
+    type_text("Python dependencies are already installed.")
 
 type_text("Please wait...")
 
