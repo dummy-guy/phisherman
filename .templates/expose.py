0c14c345c78a8fbe766ba95a87e0e20857d2400b
dummy-guy <148836695+dummy-guy@users.noreply.github.com>
2023-11-18 04:54:49 +0000

    Add files via upload

--- .templates/expose.py
+++ .templates/expose.py
@@ -2,9 +2,26 @@ import subprocess
 import time
 
 def Serveo_Link():
-    subprocess.Popen("bash serveo_link.sh", shell=True)
-    time.sleep(4)
-    with open("serveo_output.txt", "r") as file:
-        output = file.read()
-    url = output.strip().split("https://")[1]
-    return url
+    max_attempts = 3
+    attempts = 0
+    url = None
+
+    while attempts < max_attempts:
+        subprocess.Popen("bash serveo_link.sh", shell=True)
+        time.sleep(4)
+
+        with open("serveo_output.txt", "r") as file:
+            output = file.read()
+
+        if "https://" in output:
+            url = output.strip().split("https://")[1]
+            break
+        else:
+            attempts += 1
+            print(f"Serveo failed on attempt {attempts}. Retrying...")
+
+    if url is not None:
+        return url
+    else:
+        print("Serveo link retrieval failed after multiple attempts.")
+        return None
